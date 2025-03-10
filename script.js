feb4ef8b47c4251341a9d12ded4cba34009f0d89
sleptalk <64228582+sleptalk@users.noreply.github.com>
2025-03-10 18:23:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("https://slebtalk.github.io/pwa/sw.js")
+    .register("https://github.com/sleptalk/pwa/blob/main/sw.js")
     .then((reg) => console.log("Registro de SW exitoso ", reg))
     .catch((err) => console.error("Error al tratar de registrar el SW ", err));
 } else {
