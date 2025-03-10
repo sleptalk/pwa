d43296e6e9e5d2b76c6520f58b1be034b4a63509
sleptalk <64228582+sleptalk@users.noreply.github.com>
2025-03-10 10:10:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -49,23 +49,24 @@ self.addEventListener("fetch", (e) => {
 
 // Manejo de mensajes desde la aplicación principal  
 self.addEventListener("message", (e) => {  
-  if (e.data === "iniciar") {  
+  if (e.data.action === "iniciar") {  
+    const codigoConductor = e.data.codigo;  
     // Iniciar la obtención de la ubicación  
-    intervaloUbicacion = setInterval(obtenerUbicacion, 20000); // Cada 20 segundos  
-  } else if (e.data === "detener") {  
+    intervaloUbicacion = setInterval(() => obtenerUbicacion(codigoConductor), 20000); // Cada 20 segundos  
+  } else if (e.data.action === "detener") {  
     // Detener la obtención de la ubicación  
     clearInterval(intervaloUbicacion);  
   }  
 });  
 
 // Función para obtener la ubicación  
-function obtenerUbicacion() {  
+function obtenerUbicacion(codigoConductor) {  
   if ("geolocation" in navigator) {  
     navigator.geolocation.getCurrentPosition(  
       (position) => {  
         const latitud = position.coords.latitude;  
         const longitud = position.coords.longitude;  
-        enviarUbicacion(latitud, longitud);  
+        enviarUbicacion(latitud, longitud, codigoConductor);  
       },  
       (error) => {  
         console.error("Error al obtener la ubicación:", error);  
@@ -77,12 +78,12 @@ function obtenerUbicacion() {
 }  
 
 // Función para enviar la ubicación al servidor  
-function enviarUbicacion(latitud, longitud) {  
+function enviarUbicacion(latitud, longitud, codigoConductor) {  
   const url = "https://script.google.com/macros/s/AKfycbx_kg6MTahza8LJ6USXH6DMk15cE19U39IeNuXgslHdQL5zGqiW-5FIBt6gjYLumz8txg/exec";  
   const params = new URLSearchParams({  
-    codigo: "ABC123", // Reemplaza con el código del conductor  
-    latitud: latitud,  
-    longitud: longitud,  
+    codigo: codigoConductor, // Usa el código del conductor  
+    latitud: latitud, // Envía la latitud sin truncar  
+    longitud: longitud // Envía la longitud sin truncar  
   });  
 
   fetch(url + "?" + params.toString(), {  
